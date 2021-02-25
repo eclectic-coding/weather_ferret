424af1fe4f4d82420fe5b25486f97f39c20d957c
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-02-25 05:54:15 -0500

    Update rubocop configuration

--- geo_test.rb
+++ geo_test.rb
@@ -1,7 +1,6 @@
-require 'httparty'
-require 'pry'
+require "httparty"
 
-weather_url = 'https://api.openweathermap.org/data/2.5/find?q=Wilmington&NC&units=imperial&appid=3d4789029e2e08578efba398668fc1eb'
+weather_url = "https://api.openweathermap.org/data/2.5/find?q=Wilmington&NC&units=imperial&appid=3d4789029e2e08578efba398668fc1eb"
 weather = HTTParty.get(weather_url)
 puts "Latitude: #{weather['list'][0]['coord']['lat']}"
 puts "Longitude: #{weather['list'][0]['coord']['lon']}"
